8bb50e8ce1c51dfb6d0d60ac3880170c572d719c
Angelica Landazabal <angelica.informatik@gmail.com>
2021-06-16 16:39:36 -0500

    Getting data of the players from page

---
+++ NBAPlayer/views.py
@@ -0,0 +1,25 @@
+""" NBA Player views."""
+
+# Django
+from django.http import HttpResponse
+from django.shortcuts import render
+
+import requests
+import urllib, json
+
+def hello_world(request):
+    """Return a greeting"""
+
+    message = 'Hello world!'
+
+    return HttpResponse(message)
+
+
+def players_list(request):
+    """Return a greeting"""
+
+    url = 'https://mach-eight.uc.r.appspot.com/'
+    r = requests.get(url, timeout=20)
+    print(r.json())
+
+    return render(request, 'feed.html', { 'players': r.json() })
