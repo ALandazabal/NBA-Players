d2f1ea59f9634b4b8953793fb86164b4fa148f4e
Angelica Landazabal <angelica.informatik@gmail.com>
2021-07-09 04:18:05 -0500

    Cleaning the code with flake8

--- NBAPlayer/views.py
+++ NBAPlayer/views.py
@@ -1,11 +1,11 @@
 """ NBA Player views."""
 
+
 # Django
-from django.http import HttpResponse
 from django.shortcuts import render
 
 import requests
-import urllib, json
+
 
 def players_list(request):
     """Return a greeting"""
@@ -13,4 +13,4 @@ def players_list(request):
     url = 'https://mach-eight.uc.r.appspot.com/'
     r = requests.get(url, timeout=20)
 
-    return render(request, 'feed.html', { 'players_data': r.json() })
+    return render(request, 'feed.html', {'players_data': r.json()})
